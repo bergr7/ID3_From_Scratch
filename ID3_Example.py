b7eb9b1fac83c6a4a5f2a42deeed76e58e2bd81a
Bernardo Garcia del Rio <bergr7@gmail.com>
2020-12-13 13:02:12 +0100

    fix: First working version

--- ID3_Example.py
+++ ID3_Example.py
@@ -1,29 +1,43 @@
-# Let's try ID3 algorithm
-
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-import pandas as pd
 import numpy as np
+import pandas as pd
+from collections import deque
 
+# generate some data
+# define features and target values
+data = {
+    'wind_direction': ['N', 'S', 'E', 'W'],
+    'tide': ['Low', 'High'],
+    'swell_forecasting': ['small', 'medium', 'large'],
+    'good_waves': ['Yes', 'No']
+}
+
+# create an empty dataframe
+data_df = pd.DataFrame(columns=data.keys())
+
+np.random.seed(42)
+# randomnly create 1000 instances
+for i in range(1000):
+    data_df.loc[i, 'wind_direction'] = str(np.random.choice(data['wind_direction'], 1)[0])
+    data_df.loc[i, 'tide'] = str(np.random.choice(data['tide'], 1)[0])
+    data_df.loc[i, 'swell_forecasting'] = str(np.random.choice(data['swell_forecasting'], 1)[0])
+    data_df.loc[i, 'good_waves'] = str(np.random.choice(data['good_waves'], 1)[0])
+
+data_df.head()
+
+# separate target from predictors
+X = np.array(data_df.drop('good_waves', axis=1).copy())
+y = np.array(data_df['good_waves'].copy())
+feature_names = list(data_df.keys())[:3]
+
+# import and instantiate our DecisionTreeClassifier class
 from ID3 import DecisionTreeClassifier
 
-# load some data
-iris = load_iris()
-features = iris['data']
-features_ids = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
-labels = iris['target']
-labels_names = {index: name for index, name in enumerate(iris['target_names'])}
-features_df = pd.DataFrame(data=features)
-
-x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, shuffle=True)
-
-# instantiate ID3
-clf = DecisionTreeClassifier(sample=features, attributes=features_ids, labels=list(labels))
-
-# run algo
-clf.id3_recv(sample_ids=features_ids, attribute_ids=labels_names, node=None)
-
-
+# instantiate DecisionTreeClassifier
+tree_clf = DecisionTreeClassifier(X=X, feature_names=feature_names, labels=y)
+print("System entropy {:.4f}".format(tree_clf.entropy))
+# run algorithm id3 to build a tree
+tree_clf.id3()
+tree_clf.printTree()
 
 
 
