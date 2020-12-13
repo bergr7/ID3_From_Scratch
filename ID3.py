0412a42917f689bcdd20059254387e39e1ec6fb5
Bernardo Garcia del Rio <bergr7@gmail.com>
2020-12-13 13:01:30 +0100

    fix: First working version

--- ID3.py
+++ ID3.py
@@ -1,130 +1,155 @@
-# ID3 Algorithm implementation from scratch
+# ID3 (Iterative Dichotomiser 3) Algorithm implementation from scratch
 
 import math
-import numpy as np
-import pandas as pd
 from collections import deque
 
+class Node:
+    """Contains the information of the node and another nodes of the Decision Tree."""
 
-class Node(object):
     def __init__(self):
         self.value = None
         self.next = None
-        self.childs =None
+        self.childs = None
 
 
-class DecisionTreeClassifier(object):
-    def __init__(self, sample, attributes, labels):
-        self.sample = sample
-        self.attributes = attributes
+class DecisionTreeClassifier:
+    """Decision Tree Classifier using ID3 algorithm."""
+
+    def __init__(self, X, feature_names, labels):
+        self.X = X
+        self.feature_names = feature_names
         self.labels = labels
-        self.labelCodes = list(set(labels))
-        self.labelCodesCount = [labels.count(x) for x in self.labelCodes]
+        self.labelCategories = list(set(labels))
+        self.labelCategoriesCount = [list(labels).count(x) for x in self.labelCategories]
         self.node = None
-        self.entropy = self.get_entropy([x for x in range(len(self.labels))])   # calculates the initial entropy
+        self.entropy = self._get_entropy([x for x in range(len(self.labels))])  # calculates the initial entropy
 
-    def get_entropy(self, sample_ids):
+    def _get_entropy(self, x_ids):
         """ Calculates the entropy.
         Parameters
         __________
-        :param sample_ids: list, List containing the samples ID's
+        :param x_ids: list, List containing the instances ID's
         __________
         :return: entropy: float, Entropy.
         """
-        labels = [self.labels[i] for i in sample_ids]
-        label_count = [labels.count(x) for x in self.labelCodes]
-        # calculate each term of the entropy and sum them
-        entropy = sum([-x / len(sample_ids) * math.log(x / len(sample_ids), 2) if x else 0 for x in label_count])
+        # sorted labels by instance id
+        labels = [self.labels[i] for i in x_ids]
+        # count number of instances of each category
+        label_count = [labels.count(x) for x in self.labelCategories]
+        # calculate the entropy for each category and sum them
+        entropy = sum([-count / len(x_ids) * math.log(count / len(x_ids), 2) if count else 0 for count in label_count])
         return entropy
 
-    def get_information_gain(self, sample_ids, attribute_id):
+    def _get_information_gain(self, x_ids, feature_id):
         """Calculates the information gain for a given feature based on its entropy and the total entropy of the system.
         Parameters
         __________
-        :param sample_ids: list, List containing the samples ID's
-        :param attribute_id: int, attribute/feature ID
+        :param x_ids: list, List containing the instances ID's
+        :param feature_id: int, feature ID
         __________
-        :return: gain: float, the information gain for a given feature.
+        :return: info_gain: float, the information gain for a given feature.
         """
-        gain = self.get_entropy(sample_ids)
-        sample_attributes = [self.sample[x][attribute_id] for x in sample_ids]
-        attribute_vals = list(set(sample_attributes))
-        attribute_vals_count = [sample_attributes.count(x) for x in attribute_vals]
-        attribute_vals_id = [
-            sample_ids[i]
-            for i, x in enumerate(sample_attributes)
-            if x == y
-            for y in attribute_vals
+        # calculate total entropy
+        info_gain = self._get_entropy(x_ids)
+        # store in a list all the values of the chosen feature
+        x_features = [self.X[x][feature_id] for x in x_ids]
+        # get unique values
+        feature_vals = list(set(x_features))
+        # get frequency of each value
+        feature_vals_count = [x_features.count(x) for x in feature_vals]
+        # get the feature values ids
+        feature_vals_id = [
+            [x_ids[i]
+            for i, x in enumerate(x_features)
+            if x == y]
+            for y in feature_vals
         ]
-        gain = gain - sum([vc / len(sample_ids) * self.get_entropy(vids)
-                           for vc, vids in zip(attribute_vals_count, attribute_vals_id)])
-        return gain
 
-    def get_attribute_max_information_gain(self, sample_ids, attribute_ids):
+        # compute the information gain with the chosen feature
+        info_gain = info_gain - sum([val_counts / len(x_ids) * self._get_entropy(val_ids)
+                                     for val_counts, val_ids in zip(feature_vals_count, feature_vals_id)])
+
+        return info_gain
+
+    def _get_feature_max_information_gain(self, x_ids, feature_ids):
         """Finds the attribute/feature that maximizes the information gain.
         Parameters
         __________
-        :param sample_ids: list, List containing the samples ID's
-        :param attribute_ids: list, List containing the attributes ID's
+        :param x_ids: list, List containing the samples ID's
+        :param feature_ids: list, List containing the feature ID's
         __________
-        :returns: string and int, attribute and attribute id.
+        :returns: string and int, feature and feature id of the feature that maximizes the information gain
         """
-        attributes_entropy = [self.get_information_gain(sample_ids, x) for x in attribute_ids]
-        max_id = attribute_ids[attributes_entropy.index(max(attributes_entropy))]
-        return self.attributes[max_id], max_id
+        # get the entropy for each feature
+        features_entropy = [self._get_information_gain(x_ids, feature_id) for feature_id in feature_ids]
+        # find the feature that maximises the information gain
+        max_id = feature_ids[features_entropy.index(max(features_entropy))]
+
+        return self.feature_names[max_id], max_id
 
     def id3(self):
-        """Initializes ID3 algorithm.
+        """Initializes ID3 algorithm to build a Decision Tree Classifier.
 
         :return: None
         """
-        sample_ids = [x for x in range(len(self.sample))]
-        attribute_ids = [x for x in range(len(self.attributes))]
-        self.node = self.id3_recv(sample_ids, attribute_ids, self.node)
+        x_ids = [x for x in range(len(self.X))]
+        feature_ids = [x for x in range(len(self.feature_names))]
+        self.node = self._id3_recv(x_ids, feature_ids, self.node)
+        print('')
 
-    def id3_recv(self, sample_ids, attribute_ids, node):
+    def _id3_recv(self, x_ids, feature_ids, node):
+        """ID3 algorithm. It is called recursively until some criteria is met.
+        Parameters
+        __________
+        :param x_ids: list, list containing the samples ID's
+        :param feature_ids: list, List containing the feature ID's
+        :param node: object, An instance of the class Nodes
+        __________
+        :returns: An instance of the class Node containing all the information of the nodes in the Decision Tree
+        """
         if not node:
-            node = Node()   # initialize current node
-        labels_in_attributes = [self.labels[x] for x in sample_ids]
-        # if all the example have the same class
-        if len(set(labels_in_attributes)) == 1:
-            node.value = self.labels[sample_ids[0]]
-            return node     # return a leaf with that class
-        if len(attribute_ids) == 0:     # if there are not more feature to compute
-            node.value = max(set(labels_in_attributes), key=labels_in_attributes.count)     # labels mode
-            return node     # return a leaf with the most probable class
-        # else
-        # Choose the feature that maximizes the information gain for the next node
-        best_attr_name, best_attr_id = self.get_attribute_max_information_gain(sample_ids, attribute_ids)
-        node.value = best_attr_name
+            node = Node()  # initialize nodes
+        # sorted labels by instance id
+        labels_in_features = [self.labels[x] for x in x_ids]
+        # if all the example have the same class (pure node), return node
+        if len(set(labels_in_features)) == 1:
+            node.value = self.labels[x_ids[0]]
+            return node
+        # if there are not more feature to compute, return node with the most probable class
+        if len(feature_ids) == 0:
+            node.value = max(set(labels_in_features), key=labels_in_features.count)  # compute mode
+            return node
+        # else...
+        # choose the feature that maximizes the information gain
+        best_feature_name, best_feature_id = self._get_feature_max_information_gain(x_ids, feature_ids)
+        node.value = best_feature_name
         node.childs = []
-        attributes = list(set([self.sample[x][best_attr_id] for x in sample_ids]))
-        for attribute in attributes:
+        # value of the chosen feature for each instance
+        feature_values = list(set([self.X[x][best_feature_id] for x in x_ids]))
+        # loop through all the values
+        for value in feature_values:
             child = Node()
-            child.value = attribute     # add a branch from the node to each f value in out feature
-            node.childs.append(child)       # append new child node to current node
-            child_sample_ids = [x for x in sample_ids if self.sample[x][best_attr_id]]
-            if not child_sample_ids:
-                child.next = max(set(labels_in_attributes), key=labels_in_attributes.count)
+            child.value = value  # add a branch from the node to each feature value in our feature
+            node.childs.append(child)  # append new child node to current node
+            child_x_ids = [x for x in x_ids if self.X[x][best_feature_id] == value]
+            if not child_x_ids:
+                child.next = max(set(labels_in_features), key=labels_in_features.count)
+                print('')
             else:
-                if attribute_ids and best_attr_id in attribute_ids:
-                    to_remove = attribute_ids.index(best_attr_id)
-                    attribute_ids.pop(to_remove)
-                # recursively call the algo
-                child.next = self.id3_recv(child_sample_ids, attribute_ids, child.next)
+                if feature_ids and best_feature_id in feature_ids:
+                    to_remove = feature_ids.index(best_feature_id)
+                    feature_ids.pop(to_remove)
+                # recursively call the algorithm
+                child.next = self._id3_recv(child_x_ids, feature_ids, child.next)
         return node
 
     def printTree(self):
-        """It plots the decision tree.
-
-        :return: None
-        """
         if not self.node:
             return
         nodes = deque()
         nodes.append(self.node)
         while len(nodes) > 0:
-            node = node.popleft()
+            node = nodes.popleft()
             print(node.value)
             if node.childs:
                 for child in node.childs:
@@ -132,6 +157,3 @@ class DecisionTreeClassifier(object):
                     nodes.append(child.next)
             elif node.next:
                 print(node.next)
-
-
-
